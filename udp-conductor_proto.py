70b3a6017893bac8610eed26e8fdc0d0d4a02f0c
lautaronahuelp <lautaro@outlook.com>
2025-01-30 23:21:34 -0300

    Dispositivos registrados se trasladan a clase Dispositivo

--- udp-conductor_proto.py
+++ udp-conductor_proto.py
@@ -1,43 +1,66 @@
 from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol 
-from socket import *
-from copy import deepcopy
+import datetime
 
-dispositivosRegistrados = {}
+
 
 
 class Dispositivo(DatagramProtocol):
-    def __init__(self, dispReg):
-        self._dispReg = dispReg
+    def __init__(self):
+        self._dispReg = {} #DISPOSITIVOS REGISTRADOS
+        self._regPend = {} #DISPOSITIVOS REGISTRADO QUE CAMBIARON DE DIRECCION
       
 
     def datagramReceived(self, data, addr):
         #AAAA|R
         
         mensaje = data.decode("utf-8")
-        mensajeSplit = mensaje.split("|")
-        if mensajeSplit[1] == "R":
-            if not(mensajeSplit[0] in self._dispReg):
-                self._direccion = addr
-                
-                self._cuenta = mensajeSplit[0]
-                self._dispReg[mensajeSplit[0]] = {
-                                                    "cuenta": mensajeSplit[0],
-                                                    "direccion": addr,
-                                                }
-                
-                self.transport.write(("%s|R|OK" % mensajeSplit[0]).encode("utf-8"), addr)
-                print(type(self.transport))
-        if mensajeSplit[1] == "T":#tema unir con R
+
+        if mensaje == "@":#HB
+            for item in self._dispReg:
+                if(self._dispReg[item]["direccion"] == addr):
+                    self._dispReg[item]["ultimo_contacto"] = datetime.datetime.now()
+                    print(self._dispReg[item])
+                    break
             
-            if mensajeSplit[0] in self._dispReg:
-                dispositivo = self._dispReg[mensajeSplit[0]]
-                if dispositivo.getDireccion() != addr:
-                    dispositivo.setDireccion(addr)#tema si llega mensaje falso
-                self.transport.write(("%s|A" % mensajeSplit[0]).encode("utf-8"), addr)
+        else:
+            mensajeSplit = mensaje.split("|")
+
+            if mensajeSplit[1] == "R":#MENSAJE DE REGISTRO
+                if not(mensajeSplit[0] in self._dispReg):
+                    self._direccion = addr
+                    
+                    self._cuenta = mensajeSplit[0]
+                    self._dispReg[mensajeSplit[0]] = {
+                                                        "cuenta": mensajeSplit[0],
+                                                        "direccion": addr,
+                                                        "ultimo_contacto": datetime.datetime.now() 
+                                                    }
+                    
+                    self.transport.write(("%s|R|OK" % mensajeSplit[0]).encode("utf-8"), addr)
+
+            if mensajeSplit[1] == "T":#MENSAJE DE TEST
+                
+                if mensajeSplit[0] in self._dispReg:
+                    dispositivo = self._dispReg[mensajeSplit[0]]
+                    if dispositivo['direccion'] == addr:
+                        dispositivo["ultimo_contacto"] = datetime.datetime.now()
+                        self.transport.write(("%s|A" % mensajeSplit[0]).encode("utf-8"), addr)
+
+                    else:
+                        self._regPend[mensajeSplit[0]] = self._dispReg.pop(mensajeSplit[0])
+
+                    
+                    
         
         print(self._dispReg)
-        
+    
+    def enviarMensaje(self, mensaje, cuenta):
+        if cuenta in self._dispReg:
+            self.transport.write(("SERV|M|%s" % mensaje).encode("utf-8"), self._dispReg[cuenta]["direccion"])
+            return True
+        else:
+            return False
 
     def setDireccion(self, direccion):
         self._direccion = direccion
@@ -45,32 +68,24 @@ class Dispositivo(DatagramProtocol):
     def getDireccion(self):
         return self._direccion
     
-    def enviarMensaje(self, mensaje, addr):
-        self.transport.write(("SERV|M|%s" % mensaje).encode("utf-8"), addr)
-        
-    
 
 class Servidor(DatagramProtocol):
-    def __init__(self, dR, pD):
-        self._dispReg = dR
+    def __init__(self, pD):
         self._protoDisp = pD
 
     def datagramReceived(self, data, addr):
         #AAAA|M|MENSAJE
         mensaje = data.decode("utf-8")
         mensajeSplit = mensaje.split("|")
-        if mensajeSplit[0] in self._dispReg:
-            dispositivo = self._dispReg[mensajeSplit[0]]
-            self._protoDisp.enviarMensaje(mensajeSplit[2], dispositivo["direccion"])
+        
+        if self._protoDisp.enviarMensaje(mensajeSplit[2], mensajeSplit[0]):
             self.transport.write("ENVIADO A DISPOSITIVO".encode("utf-8"), addr) #VER OPCIONES DE CALLBACK
-
         else:
             self.transport.write("DISPOSITIVO NO REGISTRADO".encode("utf-8"), addr)
-        print(self._dispReg)
 
 
-protoDisp = Dispositivo(dispositivosRegistrados)
+protoDisp = Dispositivo()
 
-reactor.listenUDP(9999, Servidor(dispositivosRegistrados, protoDisp))
+reactor.listenUDP(9999, Servidor(protoDisp))
 reactor.listenUDP(3060, protoDisp)
 reactor.run()
