77a93995726b0f8274ec9f82337d144cc7eadbcd
lautaronahuelp <lautaro@outlook.com>
2025-02-01 14:11:27 -0300

    Envia mensajes en paralelo a todos los dispositivos que los tengan pendientes(de a uno por dispositivo)

--- udp-conductor_proto.py
+++ udp-conductor_proto.py
@@ -2,6 +2,10 @@ from twisted.internet import reactor, task
 from twisted.internet.protocol import DatagramProtocol 
 import datetime
 
+TIEMPO_INICIAL = datetime.datetime.strptime("10/17/1945 23:10:00", "%m/%d/%Y %H:%M:%S")
+TIMEOUT_MENSAJE = 5 #TIMEOUT EN SEGUNDO PARA REPETIR EL ENVIO DE UN MENSAJE
+TIEMPO_LOOP_ENVIA_MSJ = 0.2#CADA CUANTO SE REPITE EL LOOP DE enviaSiguiente
+
 def cbLoopDone(result):
     """
     Called when loop was stopped with success.
@@ -31,7 +35,7 @@ class Dispositivo(DatagramProtocol):
         if mensaje == "@":#HB
             for item in self._dispReg:
                 if(self._dispReg[item]["direccion"] == addr):
-                    self._dispReg[item]["ultimo_contacto"] = datetime.datetime.now()
+                    self._dispReg[item]["ultimoContacto"] = datetime.datetime.now()
                     print(self._dispReg[item])
                     break
             
@@ -46,8 +50,9 @@ class Dispositivo(DatagramProtocol):
                     self._dispReg[mensajeSplit[0]] = {
                                                         "cuenta": mensajeSplit[0],
                                                         "direccion": addr,
-                                                        "ultimo_contacto": datetime.datetime.now(),
+                                                        "ultimoContacto": datetime.datetime.now(),
                                                         "secuencia": 1,
+                                                        "mensajeAsignado": None,
                                                     }
                     
                     self.transport.write(("%s|R|OK" % mensajeSplit[0]).encode("utf-8"), addr)
@@ -57,7 +62,7 @@ class Dispositivo(DatagramProtocol):
                 if mensajeSplit[0] in self._dispReg:
                     dispositivo = self._dispReg[mensajeSplit[0]]
                     if dispositivo['direccion'] == addr:
-                        dispositivo["ultimo_contacto"] = datetime.datetime.now()
+                        dispositivo["ultimoContacto"] = datetime.datetime.now()
                         self.transport.write(("%s|A" % mensajeSplit[0]).encode("utf-8"), addr)
 
                     else:
@@ -71,36 +76,55 @@ class Dispositivo(DatagramProtocol):
         
         print(self._dispReg)
     
-    def enviarMensaje(self, mensaje, cuenta):
+    def enviarMensaje(self, mensaje):
         #SSSS|M|MENSAJE
+        cuenta = mensaje["cuenta"]
+        print("cola:" + str(self._colaMensajes))
         if cuenta in self._dispReg:#esto se controla tambien en agregarMensaje
-            self.transport.write(("%04d|M|%s" % ( self._dispReg[cuenta]["secuencia"], mensaje )).encode("utf-8"), self._dispReg[cuenta]["direccion"])
+            dispositivo = self._dispReg[cuenta]
+            dispositivo["mensajeAsignado"] = mensaje["ingreso"]#asigna mensaje a la cuenta
+            
+            self.transport.write(("%04d|M|%s" % ( dispositivo["secuencia"], mensaje["mensaje"] )).encode("utf-8"), dispositivo["direccion"])
+            mensaje["intento"] += 1
+            mensaje["ultimoIntento"] = datetime.datetime.now()
             return True
         else:
             return False
 
     def enviarSiguiente(self):
+        #buscar mensaje, comprobar si esta asignado al dispositivo o si se puede asignar(None), checkear intentos, chequear timeout
         if self._colaMensajes:
-            if self._colaMensajes[0]["intento"]  < 4:
-                self.enviarMensaje(self._colaMensajes[0]["mensaje"], self._colaMensajes[0]["cuenta"])
-                self._colaMensajes[0]["intento"] += 1
-            else:
-                 self._colaMensajes.pop()#descarta mensaje que se intento enviar 4 veces
-    
+
+            
+            for mensaje in self._colaMensajes:
+                if mensaje["intento"]  < 4:
+                    mensajeAsignado = self.getMensajeAsignado(mensaje["cuenta"])
+                    if (mensajeAsignado == mensaje["ingreso"] or mensajeAsignado == None) and (datetime.datetime.now() - mensaje["ultimoIntento"]).total_seconds() > TIMEOUT_MENSAJE:
+                        self.enviarMensaje(mensaje)
+                else:
+                    posEliminar = self._colaMensajes.index(mensaje)
+                    self._colaMensajes.pop(posEliminar)#descarta mensaje que se intento enviar 4 veces
+                    self.limMensajeAsignado(mensaje["cuenta"])
+            
+
     def reconocimientoMensaje(self, cuenta, secuencia):
+        #obtengo secuencia y mensaje asignado de la cuenta, desasigno mensaje, elimino mensaje de cuenta, y aumento secuencia
         if cuenta in self._dispReg:
             if self._dispReg[cuenta]["secuencia"] == int(secuencia):
                 if self._colaMensajes:
-                    self._colaMensajes.pop(0)#elimina el mensaje ya que secuencia coincide, corregir para multiples envios concurrentes
+                    self.eliminaMensajeCola(self._dispReg[cuenta]["mensajeAsignado"])
                     self._aumentaSecuencia(cuenta)
+                    self._dispReg[cuenta]["mensajeAsignado"] = None
 
     def agregarMensaje(self, cuenta, mensaje):
         if cuenta in self._dispReg:
             self._colaMensajes.append({ 
                                         "cuenta": cuenta,
                                         "mensaje": mensaje,
+                                        "ingreso": datetime.datetime.now(),
                                         "intento": 0,
-                                        })
+                                        "ultimoIntento": TIEMPO_INICIAL,                      
+                                    })
             return True
         else:
             return False
@@ -110,6 +134,22 @@ class Dispositivo(DatagramProtocol):
 
         if self._dispReg[cuenta]["secuencia"] > 9999:
             self._dispReg[cuenta]["secuencia"] = 1
+    
+    def eliminaMensajeCola(self, ingreso):
+        for mensaje in self._colaMensajes:
+            if mensaje["ingreso"] == ingreso:
+                self._colaMensajes.remove(mensaje)
+
+    def getMensajeAsignado(self, cuenta):
+        if self._dispReg:#esto ya lo controla agregarMensaje -> chequear si no se perdio comunicacion del dispositivo
+            #falta corroborar que exista cuenta
+            return self._dispReg[cuenta]["mensajeAsignado"]
+        else:
+            return None
+
+    def limMensajeAsignado(self, cuenta):
+        if self._dispReg:
+            self._dispReg[cuenta]["mensajeAsignado"] = None
 
     def setDireccion(self, direccion):
         self._direccion = direccion
@@ -143,7 +183,7 @@ protoServ = Servidor(protoDisp)
 
 loop = task.LoopingCall(protoDisp.enviarSiguiente)
 
-loopDeferred = loop.start(5.0)
+loopDeferred = loop.start(TIEMPO_LOOP_ENVIA_MSJ)
 
 
 # Add callbacks for stop and failure.
