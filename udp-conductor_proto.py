92632817936079a1c96ecf67df7483831b8ee8c0
lautaronahuelp <lautaro@outlook.com>
2025-01-31 21:14:01 -0300

    Mensajes del servidor se guardan en una cola y se envian de a uno, en 4 intentos hasta recibir reconocimiento

--- udp-conductor_proto.py
+++ udp-conductor_proto.py
@@ -1,15 +1,27 @@
-from twisted.internet import reactor
+from twisted.internet import reactor, task
 from twisted.internet.protocol import DatagramProtocol 
 import datetime
 
+def cbLoopDone(result):
+    """
+    Called when loop was stopped with success.
+    """
+    print("Loop done.")
+    reactor.stop()
 
 
+def ebLoopFailed(failure):
+    """
+    Called when loop execution failed.
+    """
+    print(failure.getBriefTraceback())
+    reactor.stop()
 
 class Dispositivo(DatagramProtocol):
     def __init__(self):
         self._dispReg = {} #DISPOSITIVOS REGISTRADOS
         self._regPend = {} #DISPOSITIVOS REGISTRADO QUE CAMBIARON DE DIRECCION
-      
+        self._colaMensajes = [] #MENSAJES/COMANDOS PENDIENTES DE ENVIO
 
     def datagramReceived(self, data, addr):
         #AAAA|R
@@ -34,7 +46,8 @@ class Dispositivo(DatagramProtocol):
                     self._dispReg[mensajeSplit[0]] = {
                                                         "cuenta": mensajeSplit[0],
                                                         "direccion": addr,
-                                                        "ultimo_contacto": datetime.datetime.now() 
+                                                        "ultimo_contacto": datetime.datetime.now(),
+                                                        "secuencia": 1,
                                                     }
                     
                     self.transport.write(("%s|R|OK" % mensajeSplit[0]).encode("utf-8"), addr)
@@ -50,17 +63,54 @@ class Dispositivo(DatagramProtocol):
                     else:
                         self._regPend[mensajeSplit[0]] = self._dispReg.pop(mensajeSplit[0])
 
+            if mensajeSplit[1] == "A":
+                #AAAA|A|SSSS
+                self.reconocimientoMensaje(mensajeSplit[0], mensajeSplit[2])
                     
                     
         
         print(self._dispReg)
     
     def enviarMensaje(self, mensaje, cuenta):
-        if cuenta in self._dispReg:
-            self.transport.write(("SERV|M|%s" % mensaje).encode("utf-8"), self._dispReg[cuenta]["direccion"])
+        #SSSS|M|MENSAJE
+        if cuenta in self._dispReg:#esto se controla tambien en agregarMensaje
+            self.transport.write(("%04d|M|%s" % ( self._dispReg[cuenta]["secuencia"], mensaje )).encode("utf-8"), self._dispReg[cuenta]["direccion"])
             return True
         else:
             return False
+
+    def enviarSiguiente(self):
+        if self._colaMensajes:
+            if self._colaMensajes[0]["intento"]  < 4:
+                self.enviarMensaje(self._colaMensajes[0]["mensaje"], self._colaMensajes[0]["cuenta"])
+                self._colaMensajes[0]["intento"] += 1
+            else:
+                 self._colaMensajes.pop()#descarta mensaje que se intento enviar 4 veces
+    
+    def reconocimientoMensaje(self, cuenta, secuencia):
+        if cuenta in self._dispReg:
+            if self._dispReg[cuenta]["secuencia"] == int(secuencia):
+                if self._colaMensajes:
+                    self._colaMensajes.pop()#elimina el mensaje ya que secuencia coincide, corregir para multiples envios concurrentes
+
+                self._aumentaSecuencia(cuenta)
+
+    def agregarMensaje(self, cuenta, mensaje):
+        if cuenta in self._dispReg:
+            self._colaMensajes.append({ 
+                                        "cuenta": cuenta,
+                                        "mensaje": mensaje,
+                                        "intento": 0,
+                                        })
+            return True
+        else:
+            return False
+
+    def _aumentaSecuencia(self, cuenta):
+        self._dispReg[cuenta]["secuencia"] += 1
+
+        if self._dispReg[cuenta]["secuencia"] > 9999:
+            self._dispReg[cuenta]["secuencia"] = 1
 
     def setDireccion(self, direccion):
         self._direccion = direccion
@@ -72,20 +122,35 @@ class Dispositivo(DatagramProtocol):
 class Servidor(DatagramProtocol):
     def __init__(self, pD):
         self._protoDisp = pD
+        
 
     def datagramReceived(self, data, addr):
         #AAAA|M|MENSAJE
         mensaje = data.decode("utf-8")
         mensajeSplit = mensaje.split("|")
         
-        if self._protoDisp.enviarMensaje(mensajeSplit[2], mensajeSplit[0]):
-            self.transport.write("ENVIADO A DISPOSITIVO".encode("utf-8"), addr) #VER OPCIONES DE CALLBACK
+        
+
+        if self._protoDisp.agregarMensaje(mensajeSplit[0], mensajeSplit[2]):#LOS MENSAJES DEBEN ALMACENARSE EN UNA COLA Y SE EJECUTADOS POR EL REACTOR
+            self.transport.write("AGREGADO A COLA".encode("utf-8"), addr)
         else:
             self.transport.write("DISPOSITIVO NO REGISTRADO".encode("utf-8"), addr)
+    
+    
 
 
 protoDisp = Dispositivo()
+protoServ = Servidor(protoDisp)
 
-reactor.listenUDP(9999, Servidor(protoDisp))
+loop = task.LoopingCall(protoDisp.enviarSiguiente)
+
+loopDeferred = loop.start(5.0)
+
+
+# Add callbacks for stop and failure.
+loopDeferred.addCallback(cbLoopDone)
+loopDeferred.addErrback(ebLoopFailed)
+
+reactor.listenUDP(9999, protoServ)
 reactor.listenUDP(3060, protoDisp)
 reactor.run()
